7358550447bd99c2b6bf0f8f42f67ab89c95e916
Tim Luu <timbo.luu@gmail.com>
2019-03-13 16:34:24 -0700

    Change API routes for deployment

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use('/scripts',express.static(path.resolve(__dirname, 'node_modules')))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/musicplayer/*', proxy({ target: 'http://54.242.57.180', changeOrigin: true }));
-// app.use('/api/description/*', proxy({ target: 'http://54.175.49.234', changeOrigin: true }));
-// app.use('/api/comments/*', proxy({ target: 'http://3.85.191.140', changeOrigin: true }));
-// app.use('/api/sidebar/*', proxy({ target: 'http://3.91.101.162', changeOrigin: true }));
-// app.use('/graphql', proxy({ target: 'http://3.91.101.162', changeOrigin: true }));
+app.use('/api/description/*', proxy({ target: 'http://54.175.49.234', changeOrigin: true }));
+app.use('/api/comments/*', proxy({ target: 'http:///54.80.244.244', changeOrigin: true }));
+app.use('/api/sidebar/*', proxy({ target: 'http://3.91.101.162', changeOrigin: true }));
+app.use('/graphql', proxy({ target: 'http://3.91.101.162', changeOrigin: true }));
 
 
 app.get('*', (req, res) => {
